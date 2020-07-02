d769ea1c876d31a7608e8a891fe056cfdf2a8a05
aroneia <[ejko1230@gmail.com]>
2020-07-03 01:24:58 +0900

    0702

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -28,6 +28,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.samples.petclinic.visit.Visit;
 import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.test.web.servlet.MockMvc;
@@ -63,6 +64,8 @@ class OwnerControllerTests {
 	private VisitRepository visits;
 
 	private Owner george;
+
+	
 
 	@BeforeEach
 	void setup() {
---
+++ src/test/java/org/springframework/samples/petclinic/sample/SampleControllerTest.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.petclinic.sample;
+
+
+
+public class SampleControllerTest {
+
+}
